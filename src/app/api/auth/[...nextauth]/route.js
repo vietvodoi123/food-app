328a5d50b590930d40c7425dae07b53ca2175f80
Viet_Dragon <106018834+vietvodoi123@users.noreply.github.com>
2024-09-24 18:42:37 +0700

    Update route.js

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -3,8 +3,8 @@ import GoogleProvider from "next-auth/providers/google";
 const handler = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: "612772319853-3j8cfhshii0q1vtvaa29r9octb4c7c9r.apps.googleusercontent.com",
+      clientSecret: "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",
     }),
     // FacebookProvider({
     //   clientId: process.env.FACEBOOK_CLIENT_ID,
